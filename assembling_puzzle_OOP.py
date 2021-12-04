6bf548d6172901249cf65f05dc3da2a32e5cd8f3
Raison1detre <60209329+Raison1detre@users.noreply.github.com>
2021-12-04 12:59:49 +0300

    Создана функция оценивающая сходство двух кусков картинки

--- assembling_puzzle_OOP.py
+++ assembling_puzzle_OOP.py
@@ -26,21 +26,15 @@ def read_image(path):
 
 class Tile():
     smooth_body = None
-    side_1 = None
-    side_2 = None
-    side_3 = None
-    side_4 = None
-    side_matching_to_the_side_1 = None
-    side_matching_to_the_side_2 = None
-    side_matching_to_the_side_3 = None
-    side_matching_to_the_side_4 = None
     number_of_rotate = 0
 
 
     def __init__(self, body, number):
         self.body = body
         self.number = number
-    
+        self.sides = {1:None, 2:None, 3:None, 4:None}
+        self.sides_matching_to_the_sides = {1:None, 2:None, 3:None, 4:None}
+
     def smooth(self,number_of_smoothing):
         body = self.body
         J = body.copy()
@@ -50,7 +44,7 @@ class Tile():
         self.smooth_body = J
 
     def write_image(self, path):
-        img = self.smooth_body
+        img = self.body
         h, w = img.shape[:2]
         # ppm format requires header in special format
         header = f'P3\n{w} {h}\n{MAX_VALUE}\n'
@@ -62,28 +56,36 @@ class Tile():
     def sliser(self):
         sb = self.smooth_body
         h,w = sb.shape[:2]
-        self.side_1 = sb[:,w-1,0]
+        self.sides[1] = np.array(sb[:,w-1,0], dtype=np.int16)
         sb = np.rot90(sb,1)
-        self.side_2 = sb[:,h-1,0]
+        self.sides[2] = np.array(sb[:,h-1,0], dtype=np.int16)
         sb = np.rot90(sb,1)
-        self.side_3 = sb[:,w-1,0]
+        self.sides[3] = np.array(sb[:,w-1,0], dtype=np.int16)
         sb = np.rot90(sb,1)
-        self.side_4 = sb[:,h-1,0]
+        self.sides[4] = np.array(sb[:,h-1,0], dtype=np.int16)
         sb = np.rot90(sb,1)
-        
+
+    
+def check_similarity(side1,side2):
+    similarity = []
+    for s1 in side1.sides:
+        for s2 in side2.sides:
+            s_1 = side1.sides[s1]
+            s_2 = side2.sides[s2]
+            s_2 = s_2[::-1]
+            sim = s_1 - s_2
+            similarity.append(np.std(sim))
+    print(min(similarity))
+
 
 for t in sorted(os.listdir(PATH)):
     tile = Tile(read_image(os.path.join(PATH, t)),t)
     tile.smooth(NUMBER_OF_SMOOTHING)
-    list_of_tiles.append(tile)
     tile.sliser()
+    list_of_tiles.append(tile)
     
-
-
-
 for i in range(len(list_of_tiles)):
     til = list_of_tiles[i]
     til.write_image(f"image_test{i}.ppm")
-    
-exp = list_of_tiles[0]
-print(exp.side_4)
+
+check_similarity(list_of_tiles[0],list_of_tiles[3])
