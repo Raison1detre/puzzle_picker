1626bdbf654ca321f308f9b1a98138a4c824fc50
Raison1detre <60209329+Raison1detre@users.noreply.github.com>
2021-12-16 14:10:27 +0300

    Revert "в текущем виде программа работает на всех квадратных фрагментах. Но работает катастрофически долго"
    This reverts commit b273b9cb5b3e4f8a9d0aae02f11e1aacc008f142.

--- assembling_puzzle_OOP.py
+++ assembling_puzzle_OOP.py
@@ -12,8 +12,14 @@ W = 1200
 H = 900
 CHANNEL_NUM = 3  # we work with rgb images
 MAX_VALUE = 255
+<<<<<<< HEAD
 PATH = "C:\\Users\\alex\\my-py\\data\\0000_0000_0000\\tiles" # path to the folder of tiles (not folder of folders of tiles!)
 NUMBER_OF_SMOOTHING = 5 
+=======
+PATH = "C:\\Users\\alex\\my-py\\tiles" # path to the folder of tiles (not folder of folders of tiles!)
+NUMBER_OF_SMOOTHING = 5 
+SIMILARITY_COEFFICIENT = 17
+>>>>>>> parent of b273b9c (в текущем виде программа работает на всех квадратных фрагментах. Но работает катастрофически долго)
 
 list_of_tiles = []
 
@@ -25,7 +31,6 @@ def read_image(path):
     return image
 
 class Tile():
-    number_of_false_connections = 0 # количество крайних сторон у крайних плиток которые ни с чем не связаны
     def __init__(self, body, number):
         self.body = body
         self.smooth_body = None
@@ -65,6 +70,7 @@ class Tile():
         self.sides[4] = np.array(sb[:,h-1,0], dtype=np.int16)
         sb = np.rot90(sb,1)
 
+<<<<<<< HEAD
 class Assembler():
     def __init__(self,list_of_tiles):
         dims = np.array([t.body.shape[:2] for t in list_of_tiles])
@@ -119,6 +125,51 @@ class Assembler():
             j = j[0]
         answer = similarity[:,j].flatten()
         return answer
+=======
+
+def fill_sides_matching_to_the_sides(list_of_tiles):
+    """Получает на вход список всех плиток list_of_tiles. Для каждой плитки применяет find_similar_tile(tile,side,list_of_tiles) и по результатам 
+    заполняет self.sides_matching_to_the_sides и self.rating_of_match
+    return =>> None"""
+    for tile in list_of_tiles:
+        for side in tile.sides_matching_to_the_sides:
+            if tile.sides_matching_to_the_sides[side] == None:
+                tile_match = find_similar_tile(tile,side,list_of_tiles)
+                tile.sides_matching_to_the_sides[side] = (int(tile_match[0][1]),int(tile_match[0][2]))
+                tile.rating_of_match[side] = tile_match[0][0]
+                
+def find_similar_tile(tile1, side_tile_1, list_of_tiles):
+    """Получает на вход плитку tile1, номер её стороны side_tile_1, и список всех плиток. Работает в паре с check_similarity(tile1, side_tile_1, tile2).
+    возвращает плитку и номер стороны, которая подходит к tile1, side_tile_1 наилучшим образом.
+    return =>>  np.array [коэфф.соответствия(min), номер плитки(tile2.number), номер стороны плитки(tile2.side)] """
+    similar_tiles = []
+    for tile in list_of_tiles:
+        if tile != tile1:
+            similar_tiles.append(check_similarity(tile1,side_tile_1,tile))
+    similar_tiles = np.array(similar_tiles)
+    i,j = np.where(similar_tiles == min(similar_tiles[:,0]))
+    return similar_tiles[i,:]
+
+
+def check_similarity(tile1, side_tile_1, tile2):
+    """Получает на вход плитку tile1 и номер её стороны side_tile_1, которую нужно сравнивает со всеми сторонами плитки tile2.
+    Из 4 сторон выбирает ту с которой коэфф. соответствия меньше и
+    return =>> np.array [коэфф.соответствия(min), номер плитки(tile2.number), номер стороны плитки(tile2.side)]"""
+    similarity = np.zeros((3,4))
+    count = 0
+    for s2 in tile2.sides:
+        s_1 = tile1.sides[side_tile_1]
+        s_2 = tile2.sides[s2]
+        s_2 = s_2[::-1]
+        sim = s_1 - s_2
+        similarity[0,count]=np.std(sim)
+        similarity[1,count]=tile2.number
+        similarity[2,count]=s2
+        count +=1
+    i,j = np.where(similarity == min(similarity[0,:]))
+    answer = similarity[:,j].flatten()
+    return answer
+>>>>>>> parent of b273b9c (в текущем виде программа работает на всех квадратных фрагментах. Но работает катастрофически долго)
 
 def print_image():
     for i in range(len(list_of_tiles)):
@@ -132,9 +183,14 @@ def solve():
         tile.smooth(NUMBER_OF_SMOOTHING)
         tile.sliser()
         list_of_tiles.append(tile)
+<<<<<<< HEAD
     assembler = Assembler(list_of_tiles)
     assembler.fill_sides_matching_to_the_sides(list_of_tiles)
     
+=======
+        
+    fill_sides_matching_to_the_sides(list_of_tiles)
+>>>>>>> parent of b273b9c (в текущем виде программа работает на всех квадратных фрагментах. Но работает катастрофически долго)
     for ti in list_of_tiles:
         print(ti.sides_matching_to_the_sides)
     count = 0
@@ -143,11 +199,14 @@ def solve():
             if ti.rating_of_match[key] != None:
                 count +=1
         print(ti.rating_of_match)    
+<<<<<<< HEAD
         
     print(Tile.number_of_false_connections)
     print(len(list_of_tiles))
     print(count)
     
+=======
+>>>>>>> parent of b273b9c (в текущем виде программа работает на всех квадратных фрагментах. Но работает катастрофически долго)
     #print_image()
 
 
