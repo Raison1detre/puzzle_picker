81ed35d1d096f57cbb294e5cf9578a3832fc0260
Raison1detre <60209329+Raison1detre@users.noreply.github.com>
2022-03-28 16:59:20 +0500

    Сделал жестче критерий сравнения картинок. Находит вроде как без ошибок, но функция присвоения номера не работает.  Её наддо переделывать

--- assembling_puzzle_OOP_2.py
+++ assembling_puzzle_OOP_2.py
@@ -9,6 +9,8 @@ CHANNEL_NUM = 3  # we work with rgb images
 MAX_VALUE = 255
 PATH = "C:\\Users\\alex\\my-py\\data\\0000_0000_0000\\tiles" # path to the folder of tiles (not folder of folders of tiles!)
 NUMBER_OF_SMOOTHING = 5 
+COEFFICIENT_OF_SINGLE_TON = 17 # коэффициент который определяет насколько однотонные стороны картинки будут выброшены из проверки
+COEFFICIENT_OF_SIMILARITY = 13
 
 list_of_tiles = []
 
@@ -30,8 +32,8 @@ class Tile():
         self.number_of_rotate = 0
         self.number = number
         self.sides = {1:None, 2:None, 3:None, 4:None}
-        self.sides_matching_to_the_sides = {1:None, 2:None, 3:None, 4:None}
-        self.rating_of_match = {1:None, 2:None, 3:None, 4:None}
+        self.sides_mask = {1:True, 2:True, 3:True, 4:True}
+        self.sides_before_smooth = {1:None, 2:None, 3:None, 4:None}
         self.position_in_the_picture = 0
         self.flag = False #положение флага означает была ли повернута плитка относительно начального положения, и присвоили ли ей номер.
         
@@ -55,14 +57,31 @@ class Tile():
     
     def sliser(self):
         sb = self.body
+        body = self.body
         h,w = sb.shape[:2]
+
         self.sides[1] = np.array(sb[:,w-1,:], dtype=np.int16)
+        self.sides_before_smooth[1] = np.array(body[:,w-1,:], dtype=np.int16)
+        if np.mean(np.std(self.sides_before_smooth[1],axis=0)) <  COEFFICIENT_OF_SINGLE_TON: # Это сделано чтобы монотонные стороны без выделяющихся элементов не могли пройти по цепочке дальше, я пока не знаю как фильтровать ошибку с крайними монотонными сторонами
+            self.sides_mask[1] = False
         sb = np.rot90(sb,1)
+
         self.sides[4] = np.array(sb[:,h-1,:], dtype=np.int16)
+        self.sides_before_smooth[4] = np.array(body[:,h-1,:], dtype=np.int16)
+        if np.mean(np.std(self.sides_before_smooth[4],axis=0)) <  COEFFICIENT_OF_SINGLE_TON:
+            self.sides_mask[4] = False
         sb = np.rot90(sb,1)
+
         self.sides[3] = np.array(sb[:,w-1,:], dtype=np.int16)
+        self.sides_before_smooth[3] = np.array(body[:,w-1,:], dtype=np.int16)
+        if np.mean(np.std(self.sides_before_smooth[3],axis=0)) <  COEFFICIENT_OF_SINGLE_TON:
+            self.sides_mask[3] = False
         sb = np.rot90(sb,1)
+
         self.sides[2] = np.array(sb[:,h-1,:], dtype=np.int16)
+        self.sides_before_smooth[2] = np.array(body[:,h-1,:], dtype=np.int16)
+        if np.mean(np.std(self.sides_before_smooth[2],axis=0)) <  COEFFICIENT_OF_SINGLE_TON:
+            self.sides_mask[2] = False
         sb = np.rot90(sb,1)
 
 class Assembler():
@@ -85,6 +104,8 @@ class Assembler():
         i,j = np.where(similar_tiles == min(similar_tiles[:,0]))
         j
         a = similar_tiles[i,:].flatten()
+        if a[0] > COEFFICIENT_OF_SIMILARITY:
+            return np.asarray([0,0],dtype=np.uint8)
         answer = np.asarray(a[1:],dtype=np.uint8)
         return answer
 
@@ -98,7 +119,7 @@ class Assembler():
         for s2 in tile2.sides:
             s_1 = tile1.sides[side_tile_1]
             s_2 = tile2.sides[s2]
-            if np.std(s_1) < 16 or np.std(s_2) < 16:
+            if tile1.sides_mask[side_tile_1] == False:  # Это сделано чтобы монотонные стороны не могли пройти по цепочке дальше, я пока не знаю как фильтровать ошибку с крайними монотонными сторонами
                 similarity[0,count]=255
                 similarity[1,count]=tile2.number
                 similarity[2,count]=s2
@@ -122,18 +143,33 @@ class Assembler():
         tile.smooth_body = np.rot90(tile.smooth_body,number_of_rotate)
         tile.sliser()
         tile.flag = True
-    '''
+    
     def find_number_the_tile_in_the_pickture(self,tile,list_of_tiles):
         if tile.flag==False:
             print('flag false')
-        tile_for_side_1 = self.find_similar_tile(tile, 1, list_of_tiles)
-        tile_for_side_2 = self.find_similar_tile(tile, 2, list_of_tiles)
-        tile_for_side_3 = self.find_similar_tile(tile, 3, list_of_tiles)
-        tile_for_side_4 = self.find_similar_tile(tile, 4, list_of_tiles)
+        tile_for_side = []
+        for i in range(1,5):
+            tile_for_side.append(self.find_similar_tile(tile, i, list_of_tiles))
+            if tile_for_side[i-1][1] != 0:
+                print(tile_for_side[i-1][0])
+                self.rotate(list_of_tiles[tile_for_side[i-1][0]], calculate_number_of_rotate(i, tile_for_side[i-1][1]))
+                if i == 1:
+                    list_of_tiles[tile_for_side[i-1][0]].position_in_the_picture = tile.position_in_the_picture + 1
+                elif i == 2:
+                    list_of_tiles[tile_for_side[i-1][0]].position_in_the_picture = tile.position_in_the_picture - tile.coefficient_for_find_number_of_the_tile * tile.multiplier
+                elif i == 3:
+                    list_of_tiles[tile_for_side[i-1][0]].position_in_the_picture = tile.position_in_the_picture - 1
+                elif i == 4:
+                    list_of_tiles[tile_for_side[i-1][0]].position_in_the_picture = tile.position_in_the_picture + tile.coefficient_for_find_number_of_the_tile * tile.multiplier
+        print("done!")
 
+
+
+
+    '''
         if (result1 or result4) and list_of_tiles[tile_for_side_1[0]].flag == False:
             self.rotate(list_of_tiles[tile_for_side_1[0]], calculate_number_of_rotate(1, tile_for_side_1[1]))
-            list_of_tiles[tile_for_side_1[0]].position_in_the_picture = tile.position_in_the_picture + 1
+            
             pass
           #  self.find_number_the_tile_in_the_pickture(list_of_tiles[tile_for_side_1[0]],list_of_tiles)
 
@@ -157,8 +193,8 @@ class Assembler():
 
         else:
             print("done!")
+    '''
 
-'''
 
 
 def print_image():
@@ -176,18 +212,26 @@ def solve():
     Tile.coefficient_for_find_number_of_the_tile = int((len(list_of_tiles)/12)**0.5)
     assembler = Assembler(list_of_tiles)
 
-    first_tile = list_of_tiles[5]
-    std_1 = np.std(first_tile.sides[4],axis=0)
-
-    print(std_1)
+    first_tile = list_of_tiles[0]
+    assembler.rotate(first_tile,1)
+    for instance in list_of_tiles:
+        if instance.flag == True:
+            assembler.find_number_the_tile_in_the_pickture(instance,list_of_tiles)
+    
+    for instance in list_of_tiles:
+        if instance.flag == True:
+            assembler.find_number_the_tile_in_the_pickture(instance,list_of_tiles)
+    for instance in list_of_tiles:
+        if instance.flag == True:
+            assembler.find_number_the_tile_in_the_pickture(instance,list_of_tiles)        
 
     flags = []
     positions = []
     for ex in list_of_tiles:
         positions.append(ex.position_in_the_picture)
         flags.append(ex.flag)
-    #print(positions)
-    #print(flags)
+    print(positions)
+    print(flags)
 
     
 if __name__ == "__main__":
